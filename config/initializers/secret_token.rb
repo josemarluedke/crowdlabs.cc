07ddc5dab4d07c18b55f471aa8b64bde09d72435
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-07-11 22:50:01 -0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrowdlabsCc::Application.config.secret_token = '0d949b276b0781a439479273be6c3cdbc968f146fe2cbce7ca866d4655e0340a411fbd7d7390bb61af7a8dfc5c2d0d0e36170df198bef7c14987ca60b7f32a21'
